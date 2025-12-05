5dea37ace477941f358a1d511cd14b9809960973
Qthyami <113956492+Qthyami@users.noreply.github.com>
2025-12-06 02:51:48 +0300

    Update common.api.ts
     withCredentials больше не нужны

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "/api/proxy?path=",
-  withCredentials: true,
+  baseURL: "/api/proxy?path=", 
   headers: {
     "API-KEY": "9f3854f9-1c37-4311-8912-72c5f843df71",
   },

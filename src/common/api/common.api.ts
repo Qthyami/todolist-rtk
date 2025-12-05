18f9090a34826817b5b9e674de5d6fa737132ff6
Qthyami <113956492+Qthyami@users.noreply.github.com>
2025-12-06 02:37:22 +0300

    Update common.api.ts
    base URL linked to proxy.js

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  baseURL: "/api/proxy?path=",
   withCredentials: true,
   headers: {
     "API-KEY": "9f3854f9-1c37-4311-8912-72c5f843df71",

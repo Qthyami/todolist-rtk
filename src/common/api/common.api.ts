de7b6ebcc74a1c00724442f1a102ac09915fa144
Qthyami <113956492+Qthyami@users.noreply.github.com>
2025-12-06 02:29:57 +0300

    Update common.api.ts
    new api-key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "9f3854f9-1c37-4311-8912-72c5f843df71",
   },
 });

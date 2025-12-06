72cf46a084d0db7d6ac8769fdaa3bab2f17b9366
Qthyami <113956492+Qthyami@users.noreply.github.com>
2025-12-06 03:44:33 +0300

    Update common.api.ts
    Интерсептор добавляет Bearer-токен из localStorage ко всем запросам

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,6 +1,16 @@
 import axios from "axios";
 
+// Axios instance с API-KEY
 export const instance = axios.create({
-  baseURL: "/api/proxy?path=",
-  withCredentials: true, // ОБЯЗАТЕЛЬНО для сессий и CRUD операций
+  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  headers: {
+    "API-KEY": "9f3854f9-1c37-4311-8912-72c5f843df71", // твой публичный API-KEY
+  },
+});
+
+// Интерсептор добавляет Bearer-токен из localStorage ко всем запросам
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("sn-token");
+  if (token) config.headers["Authorization"] = "Bearer " + token;
+  return config;
 });
